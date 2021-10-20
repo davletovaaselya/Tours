6c44b3eb1d72464818b6da7ae6a26664a36bc3cf
davletovaaselya <89181272+davletovaaselya@users.noreply.github.com>
2021-10-20 17:31:36 +0600

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigation from '../Navigation/Navigation'
+import Button from '../UI/Button/Button'
+import { Link } from 'react-router-dom'
+import style from './Header.module.scss'
+
+function Header() {
+    return (
+        <div className={style.headerWrapper}>
+            <Link to='/'><div><img  src='/Images/logo.png' alt='logo'/></div></Link>
+            <div className={style.navigation} >
+                <Navigation/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
